88b53ac2a1166ed5dd1b3d4584cd5fe2d0ec2574
sanoyo <sa00380304yo@gmail.com>
2020-03-21 12:46:32 +0900

    アドレス一覧ページ実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="home">
-    <Addresses/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Addresses from '@/components/Addresses.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    Addresses
-  }
+  name: 'home'
 }
 </script>
