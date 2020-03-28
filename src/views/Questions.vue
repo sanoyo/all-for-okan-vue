c38750db67e7f18fd1f10780fe7878f8095080d8
sanoyo <sa00380304yo@gmail.com>
2020-03-28 21:23:32 +0900

    動的に表示させる

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,21 +1,23 @@
 <template>
   <v-container text-center justify-center>
     <v-layout row wrap>
+      <br>
       <v-flex xs12>
         <h1>全国のおかんの質問一覧</h1>
       </v-flex>
+
       <v-flex xs12 mt-5>
         <v-card
           class="pa-md-5 mx-lg-auto"
           color="white"
           width="500px"
+          v-for="question in questions" :key="question.question"
         >
-          <v-card-title>質問</v-card-title>
+          <v-card-title>{{ question.title }}</v-card-title>
           <v-card-text>
-            {{ questions[0].content }}
+            {{ question.content }}
           </v-card-text>
         </v-card>
-        <br>
       </v-flex>
     </v-layout>
   </v-container>
