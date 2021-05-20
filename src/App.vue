84fca64dbf57d051fa0d0df92a803bafcee05493
sanoyo <sa00380304yo@gmail.com>
2021-05-21 08:06:48 +0900

    Revise Top Page

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,48 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      class="lime lighten-5"
-    >
-     <!-- <header>おかんMaster</header> -->
-      <v-tabs class="header">
-        <v-tab to="/">おかん</v-tab>
-      </v-tabs>
-      <v-flex xs12 offset-xs8>
-        <v-tab to="/topic/create" class="topic-create">トピックを作成</v-tab>
-      </v-flex>
-    </v-app-bar>
+    <v-app-bar app class="lighten-5"> </v-app-bar>
 
     <v-content>
       <v-container fluid fill-height align-start>
-        <router-view/>
+        <router-view />
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import firebase from 'firebase'
-import { mapActions } from 'vuex'
+import firebase from "firebase";
+import { mapActions } from "vuex";
 
 export default {
-  name: 'App',
-  created () {
-    firebase.auth().onAuthStateChanged(user => {
+  name: "App",
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setLoginUser(user)
-        this.fetchAddresses()
-        if (this.$router.currentRoute.name === 'home') {
-          this.$router.push({ name: 'addresses' }, () => {})
+        this.setLoginUser(user);
+        this.fetchAddresses();
+        if (this.$router.currentRoute.name === "home") {
+          this.$router.push({ name: "addresses" }, () => {});
         }
       } else {
-        this.deleteLoginUser()
+        this.deleteLoginUser();
         // questions ページ実装のため、一時的にコメントアウト
         // this.$router.push({ name: 'home' }, () => {})
       }
-    })
+    });
   },
   data: () => ({
     //
   }),
   methods: {
-    ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser', 'fetchAddresses'])
-  }
+    ...mapActions([
+      "toggleSideMenu",
+      "setLoginUser",
+      "logout",
+      "deleteLoginUser",
+      "fetchAddresses",
+    ]),
+  },
 };
 </script>
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,39 @@
 <template>
-  <v-container text-center justify-center>
-    <v-layout row wrap>
-      <v-flex xs12 mt-6>
-        おかんってどんなサイト？<br>
-        おかん思いの息子が運営する掲示板です。みなさんの知りたいことを匿名で相談できます。
-      </v-flex>
-      <v-flex xs12 mt-6>
-        <v-card
-          class="pa-md-5 mx-lg-auto"
-          color="white"
-          width="500px"
-        >
-        <v-card-title>松田聖子のコンサートどうやったらみれますか</v-card-title>
-        </v-card>
-        <br>
-        <v-card
-          class="pa-md-4 mx-lg-auto"
-          color="white"
-          width="500px"
-        >
-          <v-card-title>としちゃんんが出ている番組の一覧を見たいんだけど</v-card-title>
-        </v-card>
-        <br>
-        <v-card
-          class="pa-md-4 mx-lg-auto"
-          color="white"
-          width="500px"
-        >
-          <v-card-title>AmebaTVの接続方法を教えてください</v-card-title>
-        </v-card>
-        <br>
-      </v-flex>
-      <v-flex xs12 mt-6>
-        <v-btn
-          rounded
-          outlined
-          color="indigo"
-          x-large
-        >
-        トピックを作成してみる
-        </v-btn>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="task-item-dates">
+    <p class="task-item-dates-expires-at">
+      <span> 両親への感謝をアウトプットしましょう </span>
+    </p>
+    両親への感謝はいつでも言えると思ってませんか？
+    なにかあったときに後悔しないように日々感謝することを習慣づけましょう。
+    そうすればしぜんと感謝を伝えられるようになるはずです
+  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
   methods: {
-    ...mapActions(['login'])
-  }
-}
+    ...mapActions(["login"]),
+  },
+};
 </script>
+
+<style>
+.task-item-dates {
+  position: absolute;
+  width: 900px;
+  height: 205px;
+}
+.task-item-dates-expires-at {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 47px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #000000;
+}
+</style>
